29da3bf7fdaa92028fa5537a898f3a7a6eb688d4
Kayla Man <moonyuet2008@hotmail.com>
2026-01-12 16:51:10 +0800

    introduce the maketx extractor as being optional

--- client/ayon_substancepainter/plugins/publish/extract_maketx.py
+++ client/ayon_substancepainter/plugins/publish/extract_maketx.py
@@ -109,8 +109,9 @@ class ExtractMakeTX(publish.Extractor,
 
     label = "Extract TX"
     hosts = ["substancepainter"]
-    families = ["image"]
+    families = ["textureSet", "image"]
     settings_category = "substancepainter"
+    optional = True
 
     # Run directly after textures export
     order = publish.Extractor.order - 0.099
