02e197afc2b1974cdc6171e93ae98dbb8490da9b
Kayla Man <moonyuet2008@hotmail.com>
2026-03-09 16:56:21 +0800

    remove the optional plugin

--- client/ayon_substancepainter/plugins/publish/collect_textureset_images.py
+++ client/ayon_substancepainter/plugins/publish/collect_textureset_images.py
@@ -12,11 +12,9 @@ from ayon_substancepainter.api.lib import (
     strip_template
 )
 from ayon_core.pipeline.create import get_product_name
-from ayon_core.pipeline.publish import AYONPyblishPluginMixin
 
 
-class CollectTextureSet(pyblish.api.InstancePlugin,
-                        AYONPyblishPluginMixin):
+class CollectTextureSet(pyblish.api.InstancePlugin):
     """Extract Textures using an output template config"""
     # TODO: Production-test usage of color spaces
     # TODO: Detect what source data channels end up in each file
